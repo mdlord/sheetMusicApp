8675b3abb03d4b4a7bf4e7ca08890b285bffc0c4
mdlord <mayankdaswani123@yahoo.com>
2017-01-30 23:12:20 -0500

    phase 1 - UI

--- convert/ViewController.swift
+++ convert/ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var staffone: UIImageView!
+    @IBOutlet weak var stafftwo: UIImageView!
+    
+    
+    @IBOutlet weak var C: UIButton!
+    @IBOutlet weak var D: UIButton!
+    @IBOutlet weak var E: UIButton!
+    @IBOutlet weak var F: UIButton!
+    @IBOutlet weak var G: UIButton!
+    @IBOutlet weak var A: UIButton!
+    @IBOutlet weak var B: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        placeview()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +35,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func placeview()
+    {
+        let imageName = "note.png"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.contentMode = .scaleAspectFill
+        
+        
+        imageView.frame = CGRect(x: 150, y: 90, width: 25, height: 45)
+        view.addSubview(imageView)
+    }
 
 }
 
