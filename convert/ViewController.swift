67290cb85e819847c0414759f219eabf422114c5
mdlord <mayankdaswani123@yahoo.com>
2017-02-03 15:09:40 -0500

    piano created

--- convert/ViewController.swift
+++ convert/ViewController.swift
@@ -21,19 +21,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var G: UIButton!
     @IBOutlet weak var A: UIButton!
     @IBOutlet weak var B: UIButton!
+    @IBOutlet weak var C_h: UIButton!
     
-    var count = 0
+    var count = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //placeview()
-
-        // Do any additional setup after loading the view, typically from a nib.
+        C.backgroundColor = .clear
+        C.layer.cornerRadius = 1
+        C.layer.borderWidth = 1
+        C.layer.borderColor = UIColor.black.cgColor
+        
+        D.backgroundColor = .clear
+        D.layer.cornerRadius = 1
+        D.layer.borderWidth = 1
+        D.layer.borderColor = UIColor.black.cgColor
+        
+        E.backgroundColor = .clear
+        E.layer.cornerRadius = 1
+        E.layer.borderWidth = 1
+        E.layer.borderColor = UIColor.black.cgColor
+        
+        F.backgroundColor = .clear
+        F.layer.cornerRadius = 1
+        F.layer.borderWidth = 1
+        F.layer.borderColor = UIColor.black.cgColor
+        
+        G.backgroundColor = .clear
+        G.layer.cornerRadius = 1
+        G.layer.borderWidth = 1
+        G.layer.borderColor = UIColor.black.cgColor
+        
+        A.backgroundColor = .clear
+        A.layer.cornerRadius = 1
+        A.layer.borderWidth = 1
+        A.layer.borderColor = UIColor.black.cgColor
+        
+        B.backgroundColor = .clear
+        B.layer.cornerRadius = 1
+        B.layer.borderWidth = 1
+        B.layer.borderColor = UIColor.black.cgColor
+        
+        C_h.backgroundColor = .clear
+        C_h.layer.cornerRadius = 1
+        C_h.layer.borderWidth = 1
+        C_h.layer.borderColor = UIColor.black.cgColor
+        
+ 
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
         // Dispose of any resources that can be recreated.
     }
     
@@ -47,73 +89,74 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first{
-            let position = touch.location(in: self.stafftwo)
+            let position = touch.location(in: self.staffone)
             print(position.x)
             print(position.y)
         }
     }
+    
+    
     // note = x
 // spaces----------------------------
-    // F_l = 66
-    // A   = 52                 difference  = 12.5
-    // C_h = 38
+    // F_l = 60
+    // A   = 48                 difference  = 12.5
+    // C_h = 36
     // E_h = 24
+    // D_l =
     
 // lines-----------------------------
-    // E_l= 72
-    // G  = 58
-    // B  = 46                   difference  = 12.5
-    // D  = 32
-    // F_h= 18
+    // E_l= 66
+    // G  = 54
+    // B  = 42                  difference  = 12.5
+    // D_h= 38
+    // F_h= 30
     
+
 
     
     
     @IBAction func Cbtn(_ sender: Any) {
+        
         count += 1
-//        findypos()
-        print(count)
-//        findypos(y: <#T##Int#>)
-        placeview(xcoord: 100, ycoord: 100)
+
+        placeview(xcoord: count*75, ycoord: 78)
     }
     @IBAction func Dbtn(_ sender: Any) {
         count += 1
-//        findypos()
-        print(count)
-        placeview(xcoord: 100, ycoord: 100)
+
+        placeview(xcoord: count*75, ycoord: 72)
     }
     @IBAction func Ebtn(_ sender: Any) {
         count += 1
-//        findypos()
-        print(count)
-        placeview(xcoord: 200, ycoord: Int(72))
+
+        placeview(xcoord: count*75, ycoord: Int(66))
     }
     @IBAction func Fbtn(_ sender: Any) {
         count += 1
-//        findypos()
-        print(count)
-        
-        placeview(xcoord: 300, ycoord: Int(66))
+
+        placeview(xcoord: count*75, ycoord: Int(60))
     }
     @IBAction func Gbtn(_ sender: Any) {
         count += 1
-//        findypos()
-        print(count)
-        
-        placeview(xcoord: 400, ycoord: 58)
+
+        placeview(xcoord: count*75, ycoord: 54)
     }
     @IBAction func Abtn(_ sender: Any) {
         count += 1
-//        findypos()
-        print(count)
-        placeview(xcoord: 500, ycoord: 52)
+
+        placeview(xcoord: count*75, ycoord: 48)
     }
     @IBAction func Bbtn(_ sender: Any) {
         count += 1
-//        findypos()
-        print(count)
-        placeview(xcoord: 600, ycoord: 46)
+
+        placeview(xcoord: count*75, ycoord: 42)
     }
+    @IBAction func C_hbtn(_ sender: Any) {
+        count += 1
+        
+        placeview(xcoord: count*75, ycoord: 36)
+    }
+    
     
     // add a new parameter to change the staff if count% 10 == 0
     // fix the y coordinates and change the uiview controllers
@@ -123,34 +166,21 @@ class ViewController: UIViewController {
         let image = UIImage(named: imageName)
         let imageView = UIImageView(image: image!)
         imageView.contentMode = .scaleAspectFill
-        
-        
         imageView.frame = CGRect(x: xcoord, y: ycoord, width: 25, height: 45)
-        view.addSubview(imageView)
-    }
-    
-    func changestaff()->Bool{
-        if count%10 == 0{
-            return true
+        
+        if count<15{
+            staffone.addSubview(imageView)
+        }
+        else if count<30{
+            count = 1
+            stafftwo.addSubview(imageView)
         }
         else{
-            return false
+            print("take screenshot and remove subviews")
         }
     }
-//    
-//    func findypos(y: Int)-> Int
-//    {
-//        // difference between two staffs 205
-//        if changestaff() == true
-//        {
-//            return y
-//        }
-//    }
-//    
-    func findxpos()
-    {
     
-    }
+
 
 }
 
